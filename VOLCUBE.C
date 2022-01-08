cc590163c538d4383392bfb3baa45ec8473f198d
Rajvi Patel <97229743+Rajvi0321@users.noreply.github.com>
2022-01-08 21:10:30 +0530

    Add files via upload

---
+++ VOLCUBE.C
@@ -0,0 +1,16 @@
+ //WAP Volume of a Cube
+
+ #include<stdio.h>
+ #include<conio.h>
+
+ int main()
+{
+float l,vol;
+clrscr();
+printf("Length of a Cube is:=");
+scanf("%f",&l);
+vol=l*l*l;
+printf("\nVolume of Cube is:=%f",vol);
+getch();
+return 0;
+}
