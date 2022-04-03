983845bbc451c45699408e5a8f71f6a9ef5f5da0
KushalMehta39 <89148778+KushalMehta39@users.noreply.github.com>
2022-04-03 15:37:53 +0530

    Update CubeOfNumber.c

--- CubeOfNumber.c
+++ CubeOfNumber.c
@@ -1,1 +1,19 @@
+ //WAP Cube of a number
 
+ #include<stdio.h>
+ #include<conio.h>
+
+ int main()
+ {
+ float x,volume;
+ clrscr();
+ printf("\t\t\tCUBE OF A NUMBER");
+ printf("\nEnter x=");
+ scanf("%f",&x);
+ volume=x*x*x;
+ printf("\nCube of the number is:=%f",volume);
+ getch();
+ return 0;
+
+
+}
