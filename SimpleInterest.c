160dc6106c88e3e422ce9f85c12527c3f5bd0d85
Rajvi Patel <97229743+Rajvi0321@users.noreply.github.com>
2022-04-03 15:33:19 +0530

    Update SimpleInterest.c

--- SimpleInterest.c
+++ SimpleInterest.c
@@ -1,1 +1,21 @@
+//WAP Calculate Simple Interest
 
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+float P,R,T,SI;
+clrscr();
+printf("\t\t\tCalculate the Simple Interest");
+printf("\nEnter P =");
+scanf("%f",&P);
+printf("\nEnter R =");
+scanf("%f",&R);
+printf("\nEnter T =");
+scanf("%f",&T);
+SI=(P*R*T)/100;
+printf("\nSimple Interest=%f",SI);
+getch();
+return 0;
+}
